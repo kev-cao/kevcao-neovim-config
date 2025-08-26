9d2927c058b25e575cdbfe5a2e8e487fb7589a5d
Kevin Cao <39608887+kev-cao@users.noreply.github.com>
2025-08-26 10:46:40 -0400

    copilot: add more keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -663,6 +663,39 @@ M.copilot = {
       mode = "i",
       desc = "Accept Copilot suggestion",
     },
+    {
+      "<C-j>",
+      function()
+        local copilot = require("copilot.suggestion")
+        if copilot.is_visible() then
+          copilot.accept_line()
+        end
+      end,
+      mode = "i",
+      desc = "Accept Copilot suggestion line",
+    },
+    {
+      "<C-n>",
+      function()
+        local copilot = require("copilot.suggestion")
+        if copilot.is_visible() then
+          copilot.next()
+        end
+      end,
+      mode = "i",
+      desc = "Next Copilot suggestion",
+    },
+    {
+      "<C-p>",
+      function()
+        local copilot = require("copilot.suggestion")
+        if copilot.is_visible() then
+          copilot.prev()
+        end
+      end,
+      mode = "i",
+      desc = "Previous Copilot suggestion",
+    },
   },
 }
 
