ac56c8ab86757369f321cfe41b799be6d89b2dce
Kevin Cao <39608887+kev-cao@users.noreply.github.com>
2026-02-04 13:40:15 -0500

    fzf: toggle gitignore

--- lua/plugins/search.lua
+++ lua/plugins/search.lua
@@ -7,29 +7,42 @@ local keymaps = require("config.keymaps")
 return {
   {
     "ibhagwan/fzf-lua",
-    opts = {
-      keymap = {
-        fzf = keymaps.fzf.fzfkeys,
-      },
-      marks = {
-        marks = "[A-Za-z]",
-      },
-      fzf_opts = {
-        ["--info"] = "inline",
-        ["-i"] = true, -- case insensitive
-      },
-      winopts = {
-        preview = {
-          layout = "vertical",
+    opts = function()
+      local fzf = require("fzf-lua")
+      return {
+        keymap = {
+          fzf = keymaps.fzf.fzfkeys,
         },
-      },
-      lsp = {
-        async_or_timeout = 10000,
-      },
-      grep = {
-        formatter = "path.filename_first",
-      },
-    },
+        marks = {
+          marks = "[A-Za-z]",
+        },
+        fzf_opts = {
+          ["--info"] = "inline",
+          ["-i"] = true, -- case insensitive
+        },
+        actions = {
+          files = {
+            true,
+            ["ctrl-r"] = { fzf.actions.toggle_ignore },
+          },
+          grep = {
+            true,
+            ["ctrl-r"] = { fzf.actions.toggle_ignore },
+          }
+        },
+        winopts = {
+          preview = {
+            layout = "vertical",
+          },
+        },
+        lsp = {
+          async_or_timeout = 10000,
+        },
+        grep = {
+          formatter = "path.filename_first",
+        },
+      }
+    end,
     keys = keymaps.fzf.keys,
     config = function(_, opts)
       require("fzf-lua").setup(opts)
